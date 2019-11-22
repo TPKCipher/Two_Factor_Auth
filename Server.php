7cc632953b8057505a4efaf631cf073dc6818795
Noah Sissons <constro49@gmail.com>
2019-11-22 17:00:21 -0500

    fixed smtp

--- Server.php
+++ Server.php
@@ -1,5 +1,10 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
 if (!isset($_SESSION))
 {
     session_start();
@@ -10,7 +15,7 @@ $Email = "";
 $errors = array();
 $randVerification = "";
 $Verification = "";
-$db = mysqli_connect('localhost', 'root', '', 'software_security_twofactor');
+$db = mysqli_connect('localhost', 'root', 'newpassword', 'securityproject');
 
 if (isset($_POST['register']))
 {
@@ -40,8 +45,8 @@ if (isset($_POST['register']))
     {
 
         $password = md5($Password_1);
-        $sql = "INSERT INTO users (username, email, Password) 
-                    VALUES ('$Username', '$Email', '$password')";
+        $sql = "INSERT INTO user (username, email, password) 
+                    VALUES ('{$Username}', '{$Email}', '{$password}')";
         mysqli_query($db, $sql);
 
         $_SESSION['username'] = $Username;
@@ -73,16 +78,21 @@ if (isset($_POST['login']))
     {
         // Check for user
         $password = md5($password);
-        $query = "SELECT * FROM users WHERE username = '$Username' AND password = '$password'";
+        $query = "SELECT * FROM user WHERE username = '{$Username}' AND password = '{$password}'";
         $result = mysqli_query($db, $query);
+
 
         if (mysqli_num_rows($result) == 1)
         {
             // Send Verification email
-            $query = "SELECT email FROM users WHERE username = '$Username' AND password = '$password'"; // get email
+            $query = "SELECT email FROM user WHERE username = '{$Username}' AND password = '{$password}'"; // get email
             $result = mysqli_query($db, $query);
 
+            $Email = mysqli_fetch_array($result)['email'];
+
+
             $randVerification = rand(1000,9999);    // make code
+            $_SESSION['verificationnum'] = $randVerification;
             sendEmail($Email, $randVerification);   // Send Email
 
             // Go to verification page
@@ -97,7 +107,7 @@ if (isset($_POST['verify']))
     // Code Entered
     $Verification = mysqli_real_escape_string($db, $_POST['code']);
 
-    if ($randVerification != $Verification)
+    if ($_SESSION['verificationnum'] != $Verification)
     {
         array_push($errors, "Invalid verification code");   // Wrong Code
     }
@@ -107,6 +117,7 @@ if (isset($_POST['verify']))
         // Go to logged in state
         $_SESSION['username'] = $Username;
         $_SESSION['success'] = "You have successfully logged in.";
+        unset($_SESSION['verificationnum']);
         header('location: success.php');
     }
 }
@@ -122,21 +133,33 @@ if (isset($_GET['logout']))
 
 function sendEmail($Email, $randVerification)
 {
-    require_once('PHPMailer/PHPMailerAutoload.php');
-    $mail = new PHPMailer();
-    $mail->isSMTP();
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = 'ssl';
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = '465';
-    $mail->isHTML();
-    $mail->Username = 'codetester2000@gmail.com';
-    $mail->Password = 'SoftwareSafetySecurity360';
-    $mail->SetFrom('codetester2000@gmail.com');
-    $mail->Subject = '4 - Digit Verification';
-    $mail->Body = 'Enter this code ... '.$randVerification;
-    $mail->addAddress($Email);
-    $mail->Send();
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username = 'codetester2000@gmail.com';
+        $mail->Password = 'SoftwareSafetySecurity360';                              // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port       = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->SetFrom('codetester2000@gmail.com');
+        $mail->addAddress($Email);
+
+
+        // Content
+        //$mail->isHTML();
+        $mail->Subject = '4 - Digit Verification';
+        $mail->Body = 'Enter this code ... '.$randVerification;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
     return $randVerification;
 }
 
---
+++ masterscript.sql
@@ -0,0 +1,6 @@
+
+create table user (
+    username varchar(128),
+    password varchar(128),
+    email varchar(128)
+);
